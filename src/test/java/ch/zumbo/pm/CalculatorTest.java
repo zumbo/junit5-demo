31222a9b504daba91eda7184c0529792cc44978f
Zumstein Marcel <marcel.zumstein@bit.admin.ch>
2018-10-03 16:32:18 +0200

    Aufräumen

--- src/test/java/ch/zumbo/pm/CalculatorTest.java
+++ src/test/java/ch/zumbo/pm/CalculatorTest.java
@@ -1,22 +1,9 @@
 package ch.zumbo.pm;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.DynamicTest.dynamicTest;
-import static org.junit.jupiter.params.provider.Arguments.*;
 
 class CalculatorTest {
     private Calculator calculator;
